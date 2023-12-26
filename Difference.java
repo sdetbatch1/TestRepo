ae64351ab4886692bed056f109ce39e2babcc7e4
pvarma <pvarma@adpmn.com>
2023-12-26 06:23:46 -0500

    new files added

---
+++ Difference.java
@@ -0,0 +1,16 @@
+public class Difference {
+    public static void main(String[] args) {
+		
+		int val=Minus();
+		System.out.println(val);
+            }
+			
+			public static int Minus()
+			{
+				int a = 9;
+				int b = 5;
+				int Minus = a - b;
+				return Minus;
+
+			}
+}	
---
+++ Sum.java
@@ -0,0 +1,16 @@
+public class Sum {
+    public static void main(String[] args) {
+		
+		int val=total();
+		System.out.println(val);
+            }
+			
+			public static int total()
+			{
+				int a = 4;
+				int b = 5;
+				int total = a + b;
+				return total;
+
+			}
+}	
